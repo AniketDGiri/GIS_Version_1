7e8400c647dc2755ed3002e2e9f29313263c414f
AniketDGiri <50539447+AniketDGiri@users.noreply.github.com>
2020-12-15 14:41:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function staticLoadPlaces() {
   AFRAME.registerComponent('clickhandler', {
         init: function() {
             this.el.addEventListener('touchstart', () => {
-                alert(place.name)
+                alert('Clicked!')
             });
             //aframe over
         }});
